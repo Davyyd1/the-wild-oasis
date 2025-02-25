76e0522dd197415e967209c3e8de7a17c23fd0c0
David <d.dav889@gmail.com>
2025-02-25 21:43:41 +0200

    supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://jvoyodrlhvpizxgzmmqg.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2b3lvZHJsaHZwaXp4Z3ptbXFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MDgxMTgsImV4cCI6MjA1NjA4NDExOH0.GEsJ1mxw7_usVttUbKosBTr-9hJlFyleAiC0FoJYhrY";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
